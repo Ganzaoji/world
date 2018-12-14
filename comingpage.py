46ce13344ac6fac23faf356312b3adc75ee2fb70
Ganzaoji <45866519+Ganzaoji@users.noreply.github.com>
2018-12-14 21:42:26 +0900

    real

---
+++ comingpage.py
@@ -0,0 +1,24 @@
+#!python
+print("Content-Type: text/html")
+print()
+print('''<!doctype html>
+
+<head>
+ <title>Welcome</title>
+ <meta charset="UTF-8">
+</head>
+
+<body>
+<p style="margin-top: 40px;">
+<h1>Welcome</h1>
+ <ul>
+  <a href:"" title="무기"><li> weapon </li></a>
+  <a href:"" title="복장"><li> clothes </li></a>
+ </ul>
+</p>
+<p style="margin-top: 100px;">
+
+<h4>What's this page?</h4>
+</p>
+</body>
+''')
